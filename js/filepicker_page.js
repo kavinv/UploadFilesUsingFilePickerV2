8b979b185079d2ea2013cdf2496291a38750bd35
Kavin <kavinv@gmail.com>
2017-05-17 15:33:31 +1000

    Initial commit

---
+++ js/filepicker_page.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Kavin on 17/05/2017.
+ */
+var filepicker;
+var Save = (function () {
+    function Save(key, urlCons, whereToSaveCons) {
+        this.url = urlCons;
+        this.apiKey = key;
+        this.whereToSave = whereToSaveCons;
+    }
+    Save.prototype.dataSet = function () {
+        filepicker.setKey(this.apiKey);
+        var fileName = this.url.substr((this.url.lastIndexOf('/')) + 1);
+        filepicker.exportFile(this.url, {
+            service: this.whereToSave,
+            suggestedFilename: fileName
+        }, onSuccess, function () {
+            alert("Sorry there was an error, please try later");
+        });
+        function onSuccess(Blob) {
+            console.log(JSON.stringify(Blob));
+            alert("The file was saved successfully. This is the temporary file location : " + Blob.url);
+        }
+    };
+    return Save;
+}());
+function savingNow(destinationToSave) {
+    var urlData = document.getElementById('url').value;
+    var destination = destinationToSave.value;
+    var ds = new Save("AP9mmX5AFRgqtKpSzcr8Lz", urlData, destination);
+    return ds.dataSet();
+}
---
+++ js/filepicker_page.ts
@@ -0,0 +1,50 @@
+/**
+ * Created by Kavin on 17/05/2017.
+ */
+let filepicker;
+class Save {
+    url: any;
+    apiKey: string;
+    whereToSave: string;
+
+    constructor(key: string, urlCons: string, whereToSaveCons: string) {
+        this.url = urlCons;
+        this.apiKey = key;
+        this.whereToSave = whereToSaveCons;
+
+    }
+
+    dataSet() {
+
+        filepicker.setKey(this.apiKey);
+
+
+        let fileName = this.url.substr((this.url.lastIndexOf('/')) + 1);
+
+        filepicker.exportFile(this.url, {
+            service: this.whereToSave,
+            suggestedFilename: fileName
+        }, onSuccess, () => {
+            alert("Sorry there was an error, please try later");
+        });
+
+        function onSuccess(Blob) {
+
+            console.log(JSON.stringify(Blob));
+            alert("The file was saved successfully. This is the temporary file location : " + Blob.url);
+
+        }
+
+    }
+}
+
+
+function savingNow(destinationToSave: any): any {
+    let urlData = (<HTMLInputElement> document.getElementById('url')).value;
+    let destination = destinationToSave.value;
+    let ds = new Save("AP9mmX5AFRgqtKpSzcr8Lz", urlData, destination);
+    return ds.dataSet();
+
+}
+
+
